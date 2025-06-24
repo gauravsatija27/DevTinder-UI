7089cc8362943afb1613f8cfdcd42c4f8a9b1349
Gaurav Satija <gauravsatija1063@gmail.com>
2025-06-24 19:15:51 +0530

    built logout feature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from "react-router-dom";
 const Login = () => {
   const [emailId, setEmailId] = useState("gauravsatija1063@gmail.com");
   const [password, setPassword] = useState("Gaurav@123");
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -29,7 +30,7 @@ const Login = () => {
       dispatch(addUser(res.data));
       return navigate("/");
     } catch (error) {
-      console.error("Login failed:", error);
+      setError(error?.response?.data || "Something went wrong");
     }
   };
 
@@ -62,6 +63,7 @@ const Login = () => {
           />
         </label>
         <div className="card-actions justify-center ">
+          <p className="text-red-500">{error}</p>
           <button className="btn btn-primary" onClick={handleLogin}>
             Login
           </button>
